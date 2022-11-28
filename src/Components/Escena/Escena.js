27fd0a6404bf18f8d7ce843e95f8ce3ca1b5e75f
MariMatallana <marianamatallana@gmail.com>
2022-11-28 13:01:00 +0100

    ejercicio-1-ok

---
+++ src/Components/Escena/Escena.js
@@ -0,0 +1,19 @@
+
+export const Escena = ( props ) => {
+
+console.log (props)
+
+  return (
+    <>
+      <h1>{props.title}</h1>
+      <p> Nuestro héroe estaba flotando por el espacio sideral cuando a lo lejos divisó una nave espacial </p>
+      <p> Sentía curiosidad por el interior de la nave y se puso a inspeccionarla. Llegó a una sala con dos puertas</p>
+      <p> El héroe decidió atravesar la puerta que le llevaba a casa</p>
+      <p> Mientras tanto, otros héroes no tuvieron tanta suerte en su elección...</p>
+    
+    </>
+  );
+
+  
+}
+
