b1d85f994f33fc999c32f1bb5ff489619eb3fc4f
MariMatallana <marianamatallana@gmail.com>
2022-11-29 10:56:39 +0100

    nivel-3-medio

---
+++ src/Components/Button-style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+    width: 50%;
+    height: 50px;
+    font-size: 18px;
+    background-color: lightgray;
+
+`
---
+++ src/Components/Container-style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    justify-content: center;
+    margin:0 auto;
+    width: 90%;
+    height: 40px;
+    text-align:center;
+    background-color: white;
+    margin-top:15px;
+    border-style: solid;
+    border-color: black;
+    border: 1px solid #000000;
+    -moz-border-radius: 25px;
+    -webkit-border-radius: 25px;
+    padding: 5px;
+    
+`
