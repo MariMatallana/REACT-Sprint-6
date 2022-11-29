12a7874e5d66aacc00942a9b1b319af7543adef8
MariMatallana <marianamatallana@gmail.com>
2022-11-29 09:31:57 +0100

    ejercicio-2-ok

--- src/Components/Escena/Escena.js
+++ src/Components/Escena/Escena.js
@@ -3,13 +3,27 @@ export const Escena = ( props ) => {
 
 console.log (props)
 
+const scenes = [
+  "Nuestro héroe estaba flotando por el espacio sideral cuando a lo lejos divisó una nave espacial",
+  "Sentía curiosidad por el interior de la nave y se puso a inspeccionarla. Llegó a una sala con dos puertas.",
+  "El héroe decidió atravesar la puerta que le llevaba a casa",
+  "Mientras tanto, otros héroes no tuvieron tanta suerte en su elección..."
+]
+
+let newscenes = scenes.map(getscenes)
+
+function getscenes(e) {
+  return e
+}
+
+
   return (
     <>
       <h1>{props.title}</h1>
-      <p> Nuestro héroe estaba flotando por el espacio sideral cuando a lo lejos divisó una nave espacial </p>
-      <p> Sentía curiosidad por el interior de la nave y se puso a inspeccionarla. Llegó a una sala con dos puertas</p>
-      <p> El héroe decidió atravesar la puerta que le llevaba a casa</p>
-      <p> Mientras tanto, otros héroes no tuvieron tanta suerte en su elección...</p>
+      <p> {newscenes[0]} </p>
+      <p> {newscenes[1]} </p>
+      <p> {newscenes[2]} </p>
+      <p> {newscenes[3]} </p>
     
     </>
   );
